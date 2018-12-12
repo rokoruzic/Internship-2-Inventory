aa185880cd4216d0955849e6f63aa03520c8f225
rokoruzic <roko.ruzic4@gmail.com>
2018-12-12 22:29:21 +0100

    added 5.menu option

--- Inventory1/Program.cs
+++ Inventory1/Program.cs
@@ -177,7 +177,7 @@ namespace Inventory1
 						var regMonth = int.Parse(Console.ReadLine());
 						Console.WriteLine("Type day of registration expire date");
 						var regDay = int.Parse(Console.ReadLine());
-						var registrationExpireDate = new DateTime(regYear,regMonth,regDay);
+						var registrationExpireDate = new DateTime(regYear, regMonth, regDay);
 						Console.WriteLine("Type kilometres passed with vehicle");
 						var kilometresPassed = double.Parse(Console.ReadLine());
 						var vehicle = new Vehicle(serialNumberGuid, description, dateOfPurchase,
@@ -187,7 +187,7 @@ namespace Inventory1
 					}
 					else if (addOrDelete == "delete")
 					{
-						for (int i = 0; i <vehicleList.Count; i++)
+						for (int i = 0; i < vehicleList.Count; i++)
 						{
 							Console.WriteLine(
 								$"{i + 1}" + "\n" + $"serial number: {vehicleList[i].SerialNumber}" + "\n" +
@@ -233,7 +233,6 @@ namespace Inventory1
 								$"portable: {personalComputerList[i].Portable}");
 						}
 					}
-
 					for (int i = 0; i < mobilePhoneList.Count; i++)
 					{
 						if (serialNumberSearch == mobilePhoneList[i].SerialNumber)
@@ -270,7 +269,7 @@ namespace Inventory1
 				{
 					Console.WriteLine("Type in year to find out which PC's warranty expires that year");
 					var searchYear = int.Parse(Console.ReadLine());
-					var YearDate = new DateTime(searchYear,1,1);
+					var YearDate = new DateTime(searchYear, 1, 1);
 					for (int i = 0; i < personalComputerList.Count; i++)
 					{
 						if (personalComputerList[i].DateOfPurchase
@@ -289,49 +288,68 @@ namespace Inventory1
 						}
 					}
 				}
+				else if (menuNumber == 6)
+				{
+					int batteriesCount = 0;
+					for (int i = 0; i < personalComputerList.Count; i++)
+					{
+						if(personalComputerList[i].Batteries==true)
+						batteriesCount++;
+					}
+					for (int i = 0; i < mobilePhoneList.Count; i++)
+					{
+						if (mobilePhoneList[i].Batteries == true)
+							batteriesCount++;
+					}
+					Console.WriteLine($"there are {batteriesCount} of tech equipment with batteries");
+				}
 			}
 			while (menuNumber >= 1 && menuNumber <= 10);
-
 			Console.WriteLine("End program");
 		}
 		public static List<MobilePhone> mobilePhoneListGenerator()
 		{
 			var mobilePhoneList = new List<MobilePhone>();
-			var mobilePhone1Date = new DateTime(2007, 12,1);
+			var mobilePhone1Date = new DateTime(2007, 12, 1);
 			var mobilePhone2Date = new DateTime(2009, 1, 23);
 			var mobilePhone1 = new MobilePhone(Guid.NewGuid(), "White model with new gorilla glass",
-				mobilePhone1Date, 20, 3020.123, "Samsung", true, "0958157124", "ivo sivo");
-			var mobilePhone2= new MobilePhone(Guid.NewGuid(),"Old used phone",mobilePhone2Date,2,123,"Alcatel",true,
-				"091234234","Vojko Vrucina");
+				mobilePhone1Date, 20, 3020.123, "Samsung", false, "0958157124", "ivo sivo");
+			var mobilePhone2 = new MobilePhone(Guid.NewGuid(), "Old used phone", mobilePhone2Date, 2, 123, "Alcatel",
+				false,
+				"091234234", "Vojko Vrucina");
 			mobilePhoneList.Add(mobilePhone1);
 			mobilePhoneList.Add(mobilePhone2);
 			return mobilePhoneList;
 		}
+
 		public static List<Vehicle> vehicleListGenerator()
 		{
 			var vehicleList = new List<Vehicle>();
-			var vehicle1DateOfPurchase = new DateTime(2000,1,1);
-			var vehicle1DateOfRegExpire= new DateTime(2015,12,1);
-			var vehicle2DateOfPurchase = new DateTime(1965,11,8);
+			var vehicle1DateOfPurchase = new DateTime(2000, 1, 1);
+			var vehicle1DateOfRegExpire = new DateTime(2015, 12, 1);
+			var vehicle2DateOfPurchase = new DateTime(1965, 11, 8);
 			var vehicle2DateOfRegExpire = new DateTime(2012, 6, 19);
-			var vehicle1 = new Vehicle(Guid.NewGuid(),"Metallic black car with blue neons",vehicle1DateOfPurchase,1,100000,"Ferrari",
-				vehicle2DateOfRegExpire,525000);
-			var vehicle2 = new Vehicle(Guid.NewGuid(), "Gray timemachine with fancy doors",vehicle2DateOfPurchase,52,123456789,
-				"DeLorean",vehicle2DateOfRegExpire,5788000);
+			var vehicle1 = new Vehicle(Guid.NewGuid(), "Metallic black car with blue neons", vehicle1DateOfPurchase, 1,
+				100000, "Ferrari",
+				vehicle2DateOfRegExpire, 525000);
+			var vehicle2 = new Vehicle(Guid.NewGuid(), "Gray timemachine with fancy doors", vehicle2DateOfPurchase, 52,
+				123456789,
+				"DeLorean", vehicle2DateOfRegExpire, 5788000);
 			vehicleList.Add(vehicle1);
 			vehicleList.Add(vehicle2);
 			return vehicleList;
 		}
-
 		public static List<PersonalComputer> personalComputerListGenerator()
 		{
 			var personalComputerList = new List<PersonalComputer>();
-			var personalComputer1Date= new DateTime(2005,7,4);
-			var personalComputer2Date= new DateTime(2009,11,6);
-			var personalComputer1 = new PersonalComputer(Guid.NewGuid(),"Made of premium plastic",personalComputer1Date,24,15000,
-				"Asus",true,"Linux",true);
-			var personalComputer2 = new PersonalComputer(Guid.NewGuid(),"Brand new watercooling housing system",personalComputer2Date,
-				12,11132.123,"Intel",false,"Windows",false);
+			var personalComputer1Date = new DateTime(2005, 7, 4);
+			var personalComputer2Date = new DateTime(2009, 11, 6);
+			var personalComputer1 = new PersonalComputer(Guid.NewGuid(), "Made of premium plastic",
+				personalComputer1Date, 24, 15000,
+				"Asus", true, "Linux", true);
+			var personalComputer2 = new PersonalComputer(Guid.NewGuid(), "Brand new watercooling housing system",
+				personalComputer2Date,
+				12, 11132.123, "Intel", true, "Windows", false);
 			personalComputerList.Add(personalComputer1);
 			personalComputerList.Add(personalComputer2);
 			return personalComputerList;
@@ -344,15 +362,16 @@ namespace Inventory1
 			Console.WriteLine("1. Delete or add new personal computer ");
 			Console.WriteLine("2. Delete or add new Mobile phone ");
 			Console.WriteLine("3. Delete or add new vehicle ");
-			Console.WriteLine("4. Search through whole inventory with desired product serial number and get product info");
+			Console.WriteLine(
+				"4. Search through whole inventory with desired product serial number and get product info");
 			Console.WriteLine("5. Search trough personal computer inventory with desired warranty expire year");
 			Console.WriteLine("6. Search how many tech equipment have batteries");
 			Console.WriteLine("7. Search through mobile phone inventory with desired mobilephone brand ");
 			Console.WriteLine("8. Search through personal computer inventory with desired operating system");
 			Console.WriteLine("9. Search through mobile phone inventory with desired warranty expire year");
 			Console.WriteLine("10. Print all vehicles which registration expires in next month");
-
 		}
 	}
-	
 }
+	
+
